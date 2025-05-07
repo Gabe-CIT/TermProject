9f5e670f671872cf4837be4bab2fdeb46b64e015
Kris P <krispz.dev@gmail.com>
2025-05-07 09:33:54 -0700

    Updated scrolling function

--- scripts/services.js
+++ scripts/services.js
@@ -1,6 +1,7 @@
 const catCol = document.getElementsByClassName("cat");
 const cats = Array.from(catCol);
-const dirs = document.getElementsByClassName("directory");
+const dirCol = document.getElementsByClassName("directory");
+const dirs = Array.from(dirCol);
 const catList = document.querySelector(".cat-list");
 
 function clickScroll(eventObj) {
@@ -15,4 +16,18 @@ const toTop = document.querySelector(".to-top");
 
 toTop.addEventListener("click", function () {
     window.scrollTo(0, 0);
+});
+
+const root = document.querySelector(':root');
+const contTop = window.getComputedStyle(root).getPropertyValue('--cont-top');
+const bcitBlue = window.getComputedStyle(root).getPropertyValue('--bcit-blue');
+
+dirs.forEach((dir) => {
+    let topVal = dir.getBoundingClientRect().top;
+    let i = dirs.indexOf(dir);
+    console.log(topVal, i);
+    let styleCat = window.getComputedStyle(cats[i]);
+    if (topVal < contTop) {
+        cats[i].style.borderBottomColor = bcitBlue;
+    }
 });
