8b4bbabe6d56e3322d740d7a121ffa425341648f
Gabe <gjunio@my.bcit.ca>
2025-05-08 20:03:40 -0700

    fixed files and added test files

--- app/controllers/book_routes.py
+++ app/controllers/book_routes.py
@@ -31,7 +31,8 @@ def confirm_booking():
 
 
     # get the user id from the current user
-    appt_end_time = (datetime.strptime(request.form["appt_time"], "%H:%M") + timedelta(hours=1)).time()  # 1-hour-long appointment
+    appt_end_time = Appointments.create_end_time(request.form["appt_time"])
+    # (datetime.strptime(request.form["appt_time"], "%H:%M") + timedelta(hours=1)).time()  # 1-hour-long appointment
 
     user = db.session.execute(db.select(Users).where(Users.email == current_user.id)).scalar()
     user_email = user.email  # get the user id from the current user
@@ -39,18 +40,24 @@ def confirm_booking():
     
     # optional comment
     if request.form['appt_purpose']:
-        appt_comment = request.form['appt_comment']
+        appt_comment = request.form['appt_purpose']
     else:
         appt_comment = None
     # create new apponitment object
-    new_appt = Appointments(
-        user_id=user_email,
-        advisor_id=advisor_id,
-        date=appt_date,
-        start_time=appt_start_time,
-        end_time=appt_end_time,  # this should be updated to the end time of the appointment
-        meeting_type=appt_type,
-        comment=appt_comment
+    new_appt = Appointments.create_appointment(
+        user_email=user_email, # user email from the current user
+
+        advisor_id=advisor_id, # advisor id from the form
+        
+        date=appt_date, # date from the form
+        
+        start_time=appt_start_time, # start time from the form
+        
+        end_time=appt_end_time, # end time from the form
+        
+        meeting_type=appt_type, # appointment type from the form
+        
+        comment=appt_comment # comment from the form
     )
 
     db.session.add(new_appt)
--- app/models/appointments.py
+++ app/models/appointments.py
@@ -1,5 +1,5 @@
 from db import db
-from datetime import time, timedelta
+from datetime import datetime, timedelta
 from flask import request
 
 class Appointments(db.Model):
@@ -8,7 +8,7 @@ class Appointments(db.Model):
     id = db.mapped_column(db.Integer, nullable=False, primary_key=True)
     
     # relationship between appointments and users
-    user_id = db.mapped_column(db.Integer, db.ForeignKey("Users.id"), nullable=False)
+    user_email = db.mapped_column(db.Integer, db.ForeignKey("Users.email"), nullable=False)
     user = db.relationship("Users", back_populates="appointments")
 
     # relationship between appointments and advisors
@@ -23,3 +23,36 @@ class Appointments(db.Model):
     completed = db.mapped_column(db.Boolean, nullable=False, default=False)
     meeting_type = db.mapped_column(db.String, nullable=False)
     comment = db.mapped_column(db.String, nullable=True)
+
+    @staticmethod
+    def create_end_time(start_time):
+        # Create an end time based on the start time and duration
+        end_time = (datetime.strptime(str(start_time), "%H:%M") + timedelta(hours=1)).time()
+        return end_time
+    
+    @staticmethod
+    def create_appointment(user_email, advisor_id, date, start_time, end_time, meeting_type, comment=None):
+        # Create a new appointment object
+        new_appt = Appointments(
+            user_email=user_email,
+            advisor_id=advisor_id,
+            date=date,
+            start_time=start_time,
+            end_time=end_time,
+            meeting_type=meeting_type,
+            comment=comment
+        )
+        return new_appt
+    
+    def get_appointments_by_user(user_id):
+        # Get all appointments for a specific user
+        return db.session.execute(db.select(Appointments).where(Appointments.user_id == user_id)).scalars().all()
+    
+    def get_appointments_by_advisor(advisor_id):
+        # Get all appointments for a specific advisor
+        return db.session.execute(db.select(Appointments).where(Appointments.advisor_id == advisor_id)).scalars().all()
+    
+    def get_appointments_by_date(date):
+        # Get all appointments for a specific date
+        return db.session.execute(db.select(Appointments).where(Appointments.date == date)).scalars().all()
+    
