b25e6cc2bd5bc0f4cc930d7450396c9fb26b4b31
Additionz <ericyau_9242@hotmail.com>
2025-05-09 11:37:27 -0700

    Added booking, comment, dashboard templates with appropriate css and moved fonts to correct location.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("testforgot.html") #Change name of .html to whatever you test(insertname).html to test templates
+    return render_template("testdashboard.html") #Change name of .html to whatever you test(insertname).html to test templates
 
 if __name__ == "__main__":
     app.run(debug=True)
