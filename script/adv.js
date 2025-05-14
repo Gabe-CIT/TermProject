1e90bde193a86cf5c610067160c3da1f3866ff5c
klntz <cholotaaca@yahoo.com>
2025-05-14 01:09:18 -0700

    added changes to the user

--- script/adv.js
+++ script/adv.js
@@ -1,19 +1,14 @@
-// Function to show the modal
 function cancelAppointment() {
     document.getElementById("cancelModal").style.display = "block";
 }
 
-// Function to close the modal
 function closeModal() {
     document.getElementById("cancelModal").style.display = "none";
 }
 
-// Function to handle confirmation
 function confirmCancel() {
-    // Hide the modal
     closeModal();
 
-    // Show the cancellation message
     const cancelMessage = document.getElementById("cancel-message");
     cancelMessage.textContent = "This appointment has been cancelled.";
     cancelMessage.style.display = "block";
--- script/user.js
+++ script/user.js
@@ -1,15 +1,15 @@
 function cancelAppointment() {
-    document.getElementById("cancelModal").style.display = "flex";
-  }
-  
-  function closeModal() {
-    document.getElementById("cancelModal").style.display = "none";
-  }
-  
-  function confirmCancel() {
-    closeModal();
-    const msg = document.getElementById("cancel-message");
-    msg.style.display = "block";
-    msg.textContent = "This appointment has been cancelled.";
-  }
-  
+  document.getElementById("cancelModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("cancelModal").style.display = "none";
+}
+
+function confirmCancel() {
+  closeModal();
+
+  const cancelMessage = document.getElementById("cancel-message");
+  cancelMessage.textContent = "This appointment has been cancelled.";
+  cancelMessage.style.display = "block";
+}
