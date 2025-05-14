34971518b9ac8b5728fd69d650940690f7ac6ae9
klntz <cholotaaca@yahoo.com>
2025-05-14 11:04:48 -0700

    added logout button

---
+++ script/student.js
@@ -0,0 +1,32 @@
+function cancelAppointment() {
+  document.getElementById("cancelModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("cancelModal").style.display = "none";
+}
+
+function confirmCancel() {
+  closeModal();
+  const cancelMessage = document.getElementById("cancel-message");
+  cancelMessage.textContent = "This appointment has been cancelled.";
+  cancelMessage.style.display = "block";
+}
+
+function profileLogout() {
+  const logoutItem = document.getElementById("logout");
+  logoutItem.style.display = logoutItem.style.display === "none" ? "block" : "none";
+}
+
+function buttonLogout() {
+  document.getElementById("logoutModal").style.display = "block";
+}
+
+function closeLogoutModal() {
+  document.getElementById("logoutModal").style.display = "none";
+}
+
+function confirmLogout() {
+  closeLogoutModal();
+  window.location.href = "booking.html";
+}
